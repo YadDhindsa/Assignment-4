c97333bd0741c7a3d43094db91900b43bb22fdce
Parmeet Singh <ParmeetSingh@edu.sait.ca>
2025-03-27 22:25:29 -0600

    Update ToDoForm.jsx

--- ToDoList/components/ToDoForm.jsx
+++ ToDoList/components/ToDoForm.jsx
@@ -22,19 +22,19 @@ const ToDoForm = ({ addTask }) => {
   )
 }
 const styles = StyleSheet.create({
-    form: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    input: {
-      flex: 1,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      marginRight: 10,
-    },
-  });
-  
-  export default ToDoForm;
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default ToDoForm;
